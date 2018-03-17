8b4523a7e1516c374bf7a98d64378cbae905f1b0
khalilov075 <37265936+khalilov075@users.noreply.github.com>
2018-03-17 15:13:23 +0400

    Add files via upload

---
+++ ders/ConsoleApp8/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2-ci Chalisma..................
+            //Console.Write("Ededi daxil edin: ");
+            //string reqem= Console.ReadLine();
+            //int toplam = 0;
+            //for (int i = 0; i < reqem.Length; i++)
+            //    toplam += Convert.ToInt32(reqem[i].ToString());
+            //Console.WriteLine("Toplam:" + toplam);
+
+            //-----------------------------------------------------------
+
+            // 1ci Chalisma................
+            //int word = 0;
+            //Console.Write("Eded daxil edin: ");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //while (sayi > 0)
+            //{
+            //    word++;
+            //    sayi = sayi / 10;
+            //}
+            //Console.WriteLine("Reqeminiz--->  " + word + "  pilleli");
+            //Console.ReadKey();
+
+            //-------------------------------------------------
+
+            // 4cu Chalisma......................
+            //string cumle;
+            //string[] s;
+            //Console.Write("Cümleni daxil edin : ");
+            //cumle = Console.ReadLine();
+            //s = cumle.Split(' ');
+            //Console.WriteLine("___________________");
+            //foreach (string soz in s)
+            //{
+            //    Console.WriteLine(soz);
+            //}
+            //Console.ReadKey();
+
+            //-------------------
+            //6ci calisma
+            //Console.WriteLine("Cumleni daxil edin: ");
+            //string cumle = Console.ReadLine();
+           
+            //cumle=cumle.Replace("tenbel", "Loser");
+            //Console.WriteLine(cumle);
+            //Console.ReadKey();
+
+        }
+    }
+}
